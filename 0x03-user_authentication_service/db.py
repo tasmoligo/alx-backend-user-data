521a82714ec2400f95cee8ed37a593f3c739b84c
tasmoligo <tasieibiam28gmail.com>
2024-11-21 22:16:50 +0100

    finding and updating user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 
 from user import Base, User
 
@@ -37,3 +38,27 @@ class DB:
         self._session.add(newUser)
         self._session.commit()
         return newUser
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Returns the first row found in the users table as
+        filtered by the method’s input arguments
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user attribute
+        """
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if not hasattr(user, k):
+                raise ValueError
+            setattr(user, k, v)
+        self._session.commit()
+        return None
