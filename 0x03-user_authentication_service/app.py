7714058986f797f31b9a02cece5e9bcb4293e654
tasmoligo <tasieibiam28gmail.com>
2024-11-24 19:53:12 +0100

    basic authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,5 +29,34 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """
+    Allows auser to login
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """
+    log a user out of a session
+    """
+    session_id = request.cookies.get('session_id')
+    user = get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from db import DB
 from user import User
 from sqlalchemy.exc import NoResultFound
 from typing import Union
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -37,7 +38,7 @@ class Auth:
         else:
             raise ValueError('User {} already exists'.format(email))
 
-    def valid_login(email: str, password: str) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """
         Authenticate a user to login
         """
@@ -46,3 +47,71 @@ class Auth:
         except NoResultFound:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def _generate_uuid(self) -> str:
+        """
+        Return a string representation of a new UUID
+        """
+        return str(uuid4())
+
+    def create_session(self, email: str) -> str:
+        """
+        Returns the session ID as a string
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = self._generate_uuid()
+        return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Returns the user corrsponding to the session_id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Updates the corresponding user’s session ID to None
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            self.session_id = None
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Finds the user corresponding to the email
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Updates user's password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
